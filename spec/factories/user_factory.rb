7839d8345853b662f6084b4aecccdfe97872be40
Vlad Abacumov <vabacumov@gmail.com>
2014-12-02 22:21:54 -0800

    Adds admin flag to users

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    name "Sample_user"
+    name "username"
     email "sample@example.com"
-    password "password"
+    password "hunter2"
+    password_confirmation "hunter2"
+    factory :admin_user do
+      admin true
+    end 
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require 'capybara/rspec'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'support/authentication_helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
