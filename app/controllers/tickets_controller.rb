b092677a8bb2f86f42e4b17472b33b8ce1b10cf5
Vlad Abacumov <vabacumov@gmail.com>
2014-12-11 14:56:10 -0800

    Users can now upload 3 files at a time

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,6 +8,7 @@ class TicketsController < ApplicationController
 
   def new
     @ticket = @project.tickets.build
+    3.times { @ticket.assets.build }
   end
 
   def create
@@ -54,7 +55,7 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.require(:ticket).permit(:title, :description, :asset)
+    params.require(:ticket).permit(:title, :description, assets_attributes: [:asset])
   end
   
   def authorize_create!
