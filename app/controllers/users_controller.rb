d0dacd89f03b7c1c09c24b761afc55d666eabfe9
Vlad Abacumov <vabacumov@gmail.com>
2014-12-11 15:19:07 -0800

    Fixes the bug where new users could not sign up because of "email can't be blank" error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@
 
   private
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def set_user
